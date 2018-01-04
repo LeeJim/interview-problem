4eb52894bc3cbc1740c179e446eb23c5578e7fbe
leejim <qiulijun@bond520.com>
2018-01-04 23:52:44 +0800

    更新flatten方法

--- javascript/flatten-array.js
+++ javascript/flatten-array.js
@@ -4,10 +4,12 @@ var flattenArray = function (arr) {
 
   for (var i = 0; i < len; i++) {
     if (Array.isArray(arr[i])) {
-      res.push(...flattenArray(arr[i]))
+      res = res.concat(flattenArray(arr[i]))
     } else {
       res.push(arr[i])
     }
   }
   return res
 }
+
+console.log(flattenArray([1,[2,3],[4,[5,[6]]]]))
